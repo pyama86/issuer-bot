65dac260e30b79fcefaabe652dee67e5a09f91b4
pyama <pyama@pepabo.com>
2022-12-05 11:45:09 +0900

    should sort by updated_at with desc

--- app/services/github_repos.rb
+++ app/services/github_repos.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GithubRepos
   include ActiveModel::Model
   def self.octokit
@@ -10,15 +12,15 @@ class GithubRepos
   end
 
   def self.often_use_repos
-    amonthago = Time.now - 86400 * 30
+    amonthago = Time.now - 86_400 * 30
     octokit.organizations.map do |o|
       octokit.organization_repositories(o[:login], sort: 'updated', direction: 'desc').select do |repo|
         amonthago < repo.updated_at
       end
-    end.flatten.compact.each_with_object({}) do |o,r|
-      ora =  o[:full_name].split('/')
+    end.flatten.compact.sort_by! { |_a| r.updated_at }.reverse.each_with_object({}) do |o, r|
+      ora = o[:full_name].split('/')
       r[ora[0]] ||= []
-      r[ora[0]]  << o[:full_name]
+      r[ora[0]] << o[:full_name]
     end
   end
 end
